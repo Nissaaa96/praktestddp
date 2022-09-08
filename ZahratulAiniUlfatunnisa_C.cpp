4e2ab63ad977cd2558453c5880151ae7f104ddbd
Nissaaa96 <112962876+Nissaaa96@users.noreply.github.com>
2022-09-07 21:55:51 -1200

    Add files via upload

---
+++ ZahratulAiniUlfatunnisa_C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N , I;
+	
+	cin >> N >> I;
+	
+	cout << N + I << endl;
+	cout << N - I << endl;
+	cout << N * I << endl;
+	cout << N / I << endl;
+	cout << N % I << endl;
+	
+	return 0;
+	
+}
